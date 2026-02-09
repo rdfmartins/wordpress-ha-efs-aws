7fac8a163abd617ccd8fdba416531dae37055335
Rodolfo Martins <bonquers@gmai.com>
2026-02-09 15:37:02 -0300

    Att: HCL CODE - Módulo: Storage (EFS)

---
+++ modules/storage/main.tf
@@ -0,0 +1,92 @@
+resource "aws_efs_file_system" "this" {
+  creation_token   = var.creation_token
+  performance_mode = "generalPurpose" # Padrão recomendado [18]
+  throughput_mode  = "bursting"       # Escalabilidade automática sob demanda [18]
+  encrypted        = true             # Best Practice de segurança [19]
+
+  tags = {
+    Name    = var.efs_name
+    Project = var.project_name
+  }
+}
+
+# Mount Targets: O elo entre o EFS e as Subnets da VPC
+resource "aws_efs_mount_target" "this" {
+  count           = length(var.private_subnet_ids)
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = var.private_subnet_ids[count.index]
+  security_groups = [aws_security_group.efs.id]
+}
+
+# Security Group específico do EFS
+# Permite tráfego NFS (2049) vindo APENAS do Security Group do ECS (Chaining) [4, 17]
+resource "aws_security_group" "efs" {
+  name        = "${var.project_name}-efs-sg"
+  description = "Security Group for EFS allow NFS from ECS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "NFS from ECS Cluster"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [var.ecs_security_group_id] # Referência ao SG do Compute [4]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-efs-sg"
+  }
+}
+Arquivo: modules/storage/variables.tf
+variable "project_name" {
+  description = "Nome do projeto para tagging"
+  type        = string
+}
+
+variable "creation_token" {
+  description = "Token único para garantir idempotência na criação"
+  type        = string
+}
+
+variable "efs_name" {
+  description = "Tag Name do EFS"
+  type        = string
+  default     = "wordpress-efs-assets"
+}
+
+variable "vpc_id" {
+  description = "ID da VPC onde o EFS será montado"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Lista de IDs das Subnets onde os Mount Targets serão criados"
+  type        = list(string)
+}
+
+variable "ecs_security_group_id" {
+  description = "ID do Security Group das instâncias ECS para liberar acesso NFS (Chaining)"
+  type        = string
+}
+Arquivo: modules/storage/outputs.tf
+output "efs_id" {
+  description = "ID do Sistema de Arquivos EFS criado"
+  value       = aws_efs_file_system.this.id
+}
+
+output "efs_dns_name" {
+  description = "DNS do EFS para montagem nos containers"
+  value       = aws_efs_file_system.this.dns_name
+}
+
+output "efs_security_group_id" {
+  description = "ID do Security Group associado ao EFS"
+  value       = aws_security_group.efs.id
+}
